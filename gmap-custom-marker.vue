efce5ee1877bd7a0711f026b333521eb9a6ad706
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-09-11 17:13:27 -0400

    Only draw overlay if map is loaded

--- gmap-custom-marker.vue
+++ gmap-custom-marker.vue
@@ -12,7 +12,9 @@ export default {
   props: ['marker', 'onClick'],
   watch: {
     marker: function (val) {
-      this.$overlay.draw();
+      this.$mapPromise.then(function (map) {
+        this.$overlay.draw();
+      });
     },
   },
   provide: function () {
